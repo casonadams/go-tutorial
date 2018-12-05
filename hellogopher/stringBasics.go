6bb6a098e8c3cca5b18d70644ec2c2a7ba80c6b1
Cason Adams <casonadams@gmail.com>
2018-12-04 21:13:40 -0700

    strings and iotas

--- hellogopher/stringBasics.go
+++ hellogopher/stringBasics.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	var subject string = "Gopher"
+	subject := "Gopher"
 
 	fmt.Println("First element of Gopher string: ", string("Gopher"[0]))
 	fmt.Printf("The first value of the subject string: %v\n", string(subject[0]))
 	fmt.Printf("The last value of the subject string: %v\n", string(subject[len(subject)-1]))
+	fmt.Println("Hello " + subject + "!")
 }
