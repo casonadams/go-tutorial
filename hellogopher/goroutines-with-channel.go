34838695a89e43a12d6a7dcba5182819f21ec3ec
Cason Adams <casonadams@gmail.com>
2019-01-29 21:55:50 -0700

    go routines with channels random sleeping example

--- hellogopher/goroutines-with-channel.go
+++ hellogopher/goroutines-with-channel.go
@@ -9,14 +9,16 @@ import (
 var done chan bool = make(chan bool)
 
 func printGreetings(source string) {
+	s := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(s)
 	for i := 0; i < 9; i++ {
-		r := rand.Intn(10000)
-		fmt.Println("Hello Gopher!", i, source, r)
-		time.Sleep(time.Duration(r) * time.Millisecond)
+		w := r.Intn(1000)
+		fmt.Println("Hello Gopher!", i, source, w)
+		time.Sleep(time.Duration(w) * time.Millisecond)
 	}
 
 	if source == "goroutine" {
-		done <- true
+		done <- false // This can be true or false just needs to be sent
 	}
 }
 
